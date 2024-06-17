95a2680c5bfa71dcad39705e6efc96785658fd07
Josh Cantie <jcantie3@gmail.com>
2024-06-16 21:34:34 -0400

    split front end and back end into two different folders, got basic Vue functionality up and running.

---
+++ front-end/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <img alt="Pokeball logo" src="./assets/pokemon-svgrepo-com.png">
+        <h1>Pokemon Cards</h1>
+        <DataTable />
+    </div>
+</template>
+
+<script>
+import DataTable from './components/DataTable.vue'
+
+export default {
+    name: 'App',
+    components: {
+        DataTable,
+    }
+}
+</script>
+
+<style>
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+
+    img {
+        width: 200px;
+        height: auto;  
+    }
+</style>
