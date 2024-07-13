2f794728edcc9f283fa31270b13876a5c8596e58
Josh Cantie <jcantie3@gmail.com>
2024-07-13 15:04:07 -0400

    added new route to back-end

--- back-end/server.js
+++ back-end/server.js
@@ -32,10 +32,9 @@ app.use(cors(corsOptions));
 
 // Main get route, for grabbing all data
 app.get('/', async (req, res) => {
-    let query = "SELECT * FROM Pokemon";
+    let query = "SELECT * FROM Pokemon;";
     try {
         let results = await db.pool.query(query);
-        console.log(results);
         res.status(200).json(results[0]);
     }
     catch (err) {
@@ -44,22 +43,36 @@ app.get('/', async (req, res) => {
     }
 });
 
-// Test route
-app.post('/test', async (req, res) => {
-    
-    let query = "INSERT INTO Pokemon (pokemon_id, pokemon_name) VALUES (6, 'Garfielf');";
+// Get route for getting column headers
+app.get('/column-headers', async (req, res) => {
+    let query = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'Pokemon';";
     try {
-        await db.pool.query(query);
+        let results = await db.pool.query(query);
+        results = results[0].map((object) => object.COLUMN_NAME);
+        res.status(200).json(results);
     }
     catch (err) {
         console.error(err);
         res.sendStatus(400);
     }
-
-    // Success
-    res.redirect('/');
-
 });
+
+// // Test route
+// app.post('/test', async (req, res) => {
+    
+//     let query = "INSERT INTO Pokemon (pokemon_id, pokemon_name) VALUES (6, 'Garfielf');";
+//     try {
+//         await db.pool.query(query);
+//     }
+//     catch (err) {
+//         console.error(err);
+//         res.sendStatus(400);
+//     }
+
+//     // Success
+//     res.redirect('/');
+
+// });
 
 app.listen(PORT, () => {
     console.log(`Example app is listening on port ${PORT}.`);
