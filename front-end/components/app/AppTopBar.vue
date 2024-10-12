031626ba428f9541df8de7d1e487f23d9451041f
Josh Cantie <jcantie3@gmail.com>
2024-10-12 02:00:12 -0400

    Finally got the regular button and split button styles to match each other

--- front-end/components/app/AppTopBar.vue
+++ front-end/components/app/AppTopBar.vue
@@ -12,20 +12,34 @@
             <!-- Right Content -->
             <div class="flex items-center">
                 <Button
-                    class="mr-2 py-[9.5px] !text-custom-blue-800 bg-white hover:!bg-custom-blue-100"
                     size="small"
                     label="Add Pokemon"
                     @click="store.showAddPokemonDialog(PokemonFormMode.Add)"
+                    :pt="{
+                        root: {
+                            class: 'whitespace-nowrap mr-2 py-[9px] px-3 !text-custom-blue-800 bg-white hover:!bg-custom-blue-100 relative items-center inline-flex text-center align-bottom justify-center leading-[normal] text-sm py-1.5 px-3 gap-2 rounded-md text-primary-contrast bg-primary hover:bg-primary-emphasis transition duration-200 ease-in-out cursor-pointer overflow-hidden select-none [&>[data-pc-name=badge]]:min-w-4 [&>[data-pc-name=badge]]:h-4 [&>[data-pc-name=badge]]:leading-4',
+                        },
+                    }"
                 />
                 <SplitButton
                     :pt="{
-                        pcButton: { root: 'bg-white' },
-                        pcDropdown: { root: 'bg-white' },
+                        pcButton: {
+                            root: 'bg-white hover:!bg-custom-blue-100 relative items-center inline-flex text-center align-bottom justify-center leading-[normal] px-3 py-2 rounded-md text-primary-contrast bg-primary transition duration-200 ease-in-out cursor-pointer overflow-hidden select-none [&>[data-pc-name=badge]]:min-w-4 [&>[data-pc-name=badge]]:h-4 [&>[data-pc-name=badge]]:leading-4',
+                        },
+                        pcDropdown: {
+                            root: 'bg-white hover:!bg-custom-blue-100 relative items-center inline-flex text-center align-bottom justify-center leading-[normal] px-3 py-2 w-10 px-0 gap-0 rounded-md text-primary-contrast bg-primary transition duration-200 ease-in-out cursor-pointer overflow-hidden select-none [&>[data-pc-name=badge]]:min-w-4 [&>[data-pc-name=badge]]:h-4 [&>[data-pc-name=badge]]:leading-4',
+                        },
                     }"
                     class="mr-2"
                 >
+                    <template #dropdownicon>
+                        <i class="pi pi-angle-down text-custom-blue-800"></i>
+                    </template>
                     <span class="flex items-center">
-                        <span class="!text-custom-blue-800">More</span>
+                        <span
+                            class="!text-custom-blue-800 font-medium !text-[14px]"
+                            >More</span
+                        >
                     </span>
                 </SplitButton>
                 <IconField class="mr-2">
