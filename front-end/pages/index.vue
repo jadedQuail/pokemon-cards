f0f55c24051a90ae77eb8346a5ecf795e59c25d9
jadedQuail <jcantie3@gmail.com>
2025-04-19 21:57:52 -0400

    renamed TopBar

--- front-end/pages/index.vue
+++ front-end/pages/index.vue
@@ -3,7 +3,7 @@
         <Toast position="top-left" />
         <AppConfirmDeletion />
         <AppAddPokemonDialog />
-        <AppTopBar
+        <TopBar
             @search-change="updateFilters"
             class="fixed top-0 left-0 w-full z-[100]"
         />
@@ -19,7 +19,7 @@
 import { ref } from "vue";
 
 import AppDataTableVue from "../components/app/AppDataTable.vue";
-import AppTopBar from "../components/app/AppTopBar.vue";
+import TopBar from "../components/app/TopBar.vue";
 import AppAddPokemonDialog from "../components/app/AppAddPokemonDialog.vue";
 import AppConfirmDeletionVue from "../components/app/AppConfirmDeletion.vue";
 
