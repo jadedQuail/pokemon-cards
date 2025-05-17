1cffdf4c84eb7ebe61bc8516fbecf6dc2530d428
Josh Cantie <jcantie3@gmail.com>
2025-05-17 12:57:02 -0400

    Removed longTailwindClassNames.js and instead am in-lining the class names; this is because Tailwind's JIT compiler is cutting out these classes

--- front-end/components/TopBar.vue
+++ front-end/components/TopBar.vue
@@ -14,17 +14,17 @@
                     @click="store.showAddPokemonDialog(PokemonFormMode.Add)"
                     :pt="{
                         root: {
-                            class: buttonRootClass,
+                            class: 'whitespace-nowrap ml-4 mr-2 py-[9px] px-3 !text-custom-blue-800 bg-white hover:!bg-custom-blue-100 relative items-center inline-flex text-center align-bottom justify-center leading-[normal] text-sm py-1.5 px-3 gap-2 rounded-md text-primary-contrast bg-primary hover:bg-primary-emphasis transition duration-200 ease-in-out cursor-pointer overflow-hidden select-none [&>[data-pc-name=badge]]:min-w-4 [&>[data-pc-name=badge]]:h-4 [&>[data-pc-name=badge]]:leading-4',
                         },
                     }"
                 />
                 <SplitButton
                     :pt="{
                         pcButton: {
-                            root: splitButtonMainClass,
+                            root: 'bg-white hover:!bg-custom-blue-100 relative items-center inline-flex text-center align-bottom justify-center leading-[normal] px-3 py-2 rounded-md text-primary-contrast bg-primary transition duration-200 ease-in-out cursor-pointer overflow-hidden select-none [&>[data-pc-name=badge]]:min-w-4 [&>[data-pc-name=badge]]:h-4 [&>[data-pc-name=badge]]:leading-4',
                         },
                         pcDropdown: {
-                            root: splitButtonDropdownClass,
+                            root: 'bg-white hover:!bg-custom-blue-100 relative items-center inline-flex text-center align-bottom justify-center leading-[normal] px-3 py-2 w-10 px-0 gap-0 rounded-md text-primary-contrast bg-primary transition duration-200 ease-in-out cursor-pointer overflow-hidden select-none [&>[data-pc-name=badge]]:min-w-4 [&>[data-pc-name=badge]]:h-4 [&>[data-pc-name=badge]]:leading-4',
                         },
                     }"
                     class="mr-2"
@@ -64,7 +64,7 @@
                     @click="store.showLoginDialog()"
                     :pt="{
                         root: {
-                            class: loginButtonClass,
+                            class: 'whitespace-nowrap ml-4 mr-2 py-[9px] px-3 !text-custom-blue-800 bg-white hover:!bg-custom-blue-100 relative items-center inline-flex text-center align-bottom justify-center leading-[normal] text-sm py-1.5 px-3 rounded-md text-primary-contrast bg-primary hover:bg-primary-emphasis transition duration-200 ease-in-out cursor-pointer overflow-hidden select-none [&>[data-pc-name=badge]]:min-w-4 [&>[data-pc-name=badge]]:h-4 [&>[data-pc-name=badge]]:leading-4',
                         },
                     }"
                 />
@@ -79,12 +79,6 @@ import SplitButton from "primevue/splitbutton";
 import { FilterMatchMode } from "@primevue/core/api";
 import { useStore } from "~/store/store.js";
 import { PokemonFormMode, CategoriesFormMode } from "~/static/constants.js";
-import {
-    buttonRootClass,
-    splitButtonMainClass,
-    splitButtonDropdownClass,
-    loginButtonClass,
-} from "~/static/longTailwindClassNames.js";
 
 const emit = defineEmits(["search-change"]);
 const store = useStore();
