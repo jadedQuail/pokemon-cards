df40b910c56d150e7b298d12f5be27a94bbf1b47
Josh Cantie <jcantie3@gmail.com>
2025-06-01 16:45:24 -0400

    upped the max attempts on the register limiter, because it was too easy to f-up your password

--- back-end/middleware/registerLimiter.js
+++ back-end/middleware/registerLimiter.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const registerLimiter = rateLimit({
     windowMs: 60 * 1000,
-    max: 3,
+    max: 10,
     message: {
         error: "Too many registration attempts. Please try again later.",
     },
