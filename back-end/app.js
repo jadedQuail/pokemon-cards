d542d6769b19f42c1d9b1cfdca47b7dea9bb9bda
Josh Cantie <jcantie3@gmail.com>
2025-06-04 22:18:30 -0400

    split server.js into server and app files; installed jest and supertest

---
+++ back-end/app.js
@@ -0,0 +1,35 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+import pokemonRoutes from "./routes/pokemonRoutes.js";
+import typeRoutes from "./routes/typeRoutes.js";
+import setRoutes from "./routes/setRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+
+const app = express();
+
+const ALLOW_ALL_ORIGINS = process.env.ALLOW_ALL_ORIGINS === "true";
+const ALLOW_LIST = [process.env.FRONTEND];
+
+// TODO: Create a unit test and journey tests for this app
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (ALLOW_ALL_ORIGINS || ALLOW_LIST.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.use("/pokemon", pokemonRoutes);
+app.use("/types", typeRoutes);
+app.use("/sets", setRoutes);
+app.use("/auth", authRoutes);
+
+export default app;
--- back-end/server.js
+++ back-end/server.js
@@ -1,37 +1,6 @@
-import "dotenv/config";
-import express from "express";
-import cors from "cors";
-
-import pokemonRoutes from "./routes/pokemonRoutes.js";
-import typeRoutes from "./routes/typeRoutes.js";
-import setRoutes from "./routes/setRoutes.js";
-import authRoutes from "./routes/authRoutes.js";
-
-const app = express();
+import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
-const ALLOW_ALL_ORIGINS = process.env.ALLOW_ALL_ORIGINS === "true";
-const ALLOW_LIST = [process.env.FRONTEND];
-
-// TODO: Create a unit test and journey tests for this app
-
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (ALLOW_ALL_ORIGINS || ALLOW_LIST.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-};
-
-app.use(cors(corsOptions));
-app.use(express.json());
-
-app.use("/pokemon", pokemonRoutes);
-app.use("/types", typeRoutes);
-app.use("/sets", setRoutes);
-app.use("/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Example app is listening on port ${PORT}.`);
