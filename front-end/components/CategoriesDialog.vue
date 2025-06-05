bda718dddb7b3e2728497fde0e7502ce7692697a
Josh Cantie <jcantie3@gmail.com>
2025-06-04 22:05:54 -0400

    removed shallow copy of sets and types that existed in CategoriesDialog

--- front-end/components/CategoriesDialog.vue
+++ front-end/components/CategoriesDialog.vue
@@ -50,7 +50,7 @@
                         "
                     >
                         <div
-                            v-for="type in types"
+                            v-for="type in categoryStore.types"
                             :key="type"
                             class="flex items-center gap-2"
                         >
@@ -71,7 +71,7 @@
                         "
                     >
                         <div
-                            v-for="set in sets"
+                            v-for="set in categoryStore.sets"
                             :key="set"
                             class="flex items-center gap-2"
                         >
@@ -96,49 +96,15 @@ import { ref } from "vue";
 import { usePokemonStore } from "~/stores/pokemonStore.js";
 import { useCategoryStore } from "~/stores/categoryStore.js";
 import { CategoriesFormMode } from "~/static/constants.js";
-import {
-    getTypeOptions,
-    deleteType,
-    addType,
-} from "@/services/apiClient/type.js";
-import { getSetOptions, addSet, deleteSet } from "@/services/apiClient/set.js";
+import { deleteType, addType } from "@/services/apiClient/type.js";
+import { addSet, deleteSet } from "@/services/apiClient/set.js";
 
 const config = useRuntimeConfig();
-
 const pokemonStore = usePokemonStore();
 const categoryStore = useCategoryStore();
 
-// TODO: Make these types and sets store objects so they can be used both here and on AddPokemonDialog
-const types = ref([]);
-const sets = ref([]);
-
 const newCategoryToAdd = ref("");
-
 const selectedCategories = ref([]);
-
-const loadTypeOptions = async () => {
-    try {
-        const apiUrl = config.public.API_URL;
-        types.value = await getTypeOptions(apiUrl);
-    } catch (error) {
-        console.error(
-            'Error fetching type options for "Categories Dialog":',
-            error
-        );
-    }
-};
-
-const loadSetOptions = async () => {
-    try {
-        const apiUrl = config.public.API_URL;
-        sets.value = await getSetOptions(apiUrl);
-    } catch (error) {
-        console.error(
-            'Error fetching set options for "Categories Dialog":',
-            error
-        );
-    }
-};
 
 const resetDialogInput = async () => {
     newCategoryToAdd.value = "";
@@ -190,7 +156,7 @@ const addCategory = async (categoryName) => {
     }
 
     await resetDialogInput();
-    await refreshCategories();
+    await categoryStore.refreshCategories(apiUrl);
     await pokemonStore.fetchPokemonData(apiUrl);
 };
 
@@ -217,7 +183,7 @@ const removeCategories = async () => {
     }
 
     await resetDialogInput();
-    await refreshCategories();
+    await categoryStore.refreshCategories(apiUrl);
     await pokemonStore.fetchPokemonData(apiUrl);
 };
 
@@ -225,12 +191,8 @@ const removeCategoriesDisabled = computed(() => {
     return selectedCategories.value.length === 0;
 });
 
-const refreshCategories = async () => {
-    await loadTypeOptions();
-    await loadSetOptions();
-};
-
 onMounted(async () => {
-    refreshCategories();
+    const apiUrl = config.public.API_URL;
+    await categoryStore.refreshCategories(apiUrl);
 });
 </script>
