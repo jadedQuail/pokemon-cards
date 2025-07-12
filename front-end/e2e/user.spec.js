48131e50fe76f13f21a288c5dd726ec612822a4b
Josh Cantie <jcantie3@gmail.com>
2025-07-12 13:03:12 -0400

    updated test so we're using an established test admin user instead of creating and deleting a user each time; updated documentation

--- front-end/e2e/user.spec.js
+++ front-end/e2e/user.spec.js
@@ -1,39 +1,24 @@
 import { test, expect } from "@playwright/test";
-import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
 
 const API_URL = process.env.API_URL;
-const ADMIN_API_KEY = process.env.ADMIN_API_KEY;
-const USERNAME = "AfGk3LmN75";
-const PASSWORD = "Welcome2025!";
+const ADMIN_USERNAME = process.env.TEST_ADMIN_USERNAME;
+const ADMIN_PASSWORD = process.env.TEST_ADMIN_PASSWORD;
 
 test.describe("user lifecycle suite", () => {
-    let userId;
+    let adminToken;
 
     test.beforeAll(async ({ request }) => {
-        // Create User
-        const createRes = await request.post(`${API_URL}/auth/create-user`, {
+        const loginResponse = await request.post(`${API_URL}/auth/login`, {
             data: {
-                username: USERNAME,
-                password: PASSWORD,
-                confirmPassword: PASSWORD,
+                username: ADMIN_USERNAME,
+                password: ADMIN_PASSWORD,
             },
         });
-        expect(createRes.status()).toBe(201);
-
-        // Log User In
-        const loginRes = await request.post(`${API_URL}/auth/login`, {
-            data: {
-                username: USERNAME,
-                password: PASSWORD,
-            },
-        });
-        expect(loginRes.status()).toBe(200);
-        const { token } = await loginRes.json();
-
-        const decoded = jwt.decode(token);
-        userId = decoded.id;
+        expect(loginResponse.status()).toBe(200);
+        const { token } = await loginResponse.json();
+        adminToken = token;
     });
 
     test("able to login", async ({ page }) => {
@@ -41,18 +26,10 @@ test.describe("user lifecycle suite", () => {
 
         await page.getByTestId("login-logout-button").click();
 
-        await page.getByTestId("login-dialog-username").fill(USERNAME);
+        await page.getByTestId("login-dialog-username").fill(ADMIN_USERNAME);
         await page
             .getByTestId("login-dialog-password-parent")
             .locator("input")
-            .fill(PASSWORD);
-    });
-
-    test.afterAll(async ({ request }) => {
-        const deleteRes = await request.delete(
-            `${API_URL}/auth/delete/${userId}`,
-            { headers: { "x-api-key": ADMIN_API_KEY } }
-        );
-        expect(deleteRes.status()).toBe(204);
+            .fill(ADMIN_PASSWORD);
     });
 });
