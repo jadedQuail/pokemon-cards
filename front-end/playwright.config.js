d786ee61ad4a62ebf1159e432f20bf0e864264c4
Josh Cantie <jcantie3@gmail.com>
2025-07-23 22:36:20 -0400

    took playwright out of headed mode; adjusted some spacing in the config

--- front-end/playwright.config.js
+++ front-end/playwright.config.js
@@ -1,30 +1,17 @@
 import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-    // Look for test files in the "tests" directory, relative to this configuration file.
     testDir: "e2e",
-
-    // Run all tests in parallel.
     fullyParallel: true,
-
-    // Opt out of parallel tests on CI.
     workers: 1,
-
-    // Reporter to use
     reporter: "list",
-
     use: {
-        // Base URL to use in actions like `await page.goto('/')`.
         baseURL: "http://localhost:3000",
-
-        // Collect trace when retrying the failed test.
         trace: "on-first-retry",
-
         launchOptions: {
             slowMo: 0,
         },
     },
-    // Configure projects for major browsers.
     projects: [
         {
             name: "chromium",
