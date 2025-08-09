68ca00f1cb311a3d4f9da8b53baa95eb54c5fd66
Josh Cantie <jcantie3@gmail.com>
2025-08-09 14:00:08 -0400

    adding some log statements to debug my playwright tests failing on Github

--- front-end/e2e/utils/login.js
+++ front-end/e2e/utils/login.js
@@ -4,12 +4,23 @@ dotenv.config();
 const BASE_URL = process.env.BASE_URL;
 
 export async function logUserIn(page, username, password) {
+    console.log("Logging in to:", BASE_URL);
+
     await page.goto(BASE_URL);
+    console.log("Page loaded");
+
     await page.getByTestId("login-logout-button").click();
+    console.log("Clicked login button");
+
     await page.getByTestId("login-dialog-username").fill(username);
+    console.log("Filled username");
+
     await page
         .getByTestId("login-dialog-password-parent")
         .locator("input")
         .fill(password);
+    console.log("Filled password");
+
     await page.getByTestId("login-submit-button").click();
+    console.log("Clicked submit");
 }
