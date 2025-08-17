f4b417dc3a39af924cfefbc02b795af865b1b2ad
jadedQuail <jcantie3@gmail.com>
2025-08-17 12:58:48 -0400

    removed a screen.debug call in a test that was unnecessary; actually committing commenting out of problematic deploy step

--- front-end/tests/CategoriesDialog.test.js
+++ front-end/tests/CategoriesDialog.test.js
@@ -47,7 +47,6 @@ beforeEach(async () => {
 
 test("adds a new type and updates the store", async () => {
     await renderSuspended(CategoriesDialog);
-    screen.debug(screen.getByTestId("categories-form"));
 
     await fireEvent.update(screen.getByTestId("new-category-input"), "Dragon");
     await fireEvent.submit(screen.getByTestId("categories-form"));
